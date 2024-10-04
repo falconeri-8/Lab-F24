177e27cdf2e39701ebab94092ad6ab2cf5cd13ee
falconeri <hananeight@gmail.com>
2024-10-04 20:25:08 +0500

    updated 04/6

--- 04/Task6.cpp
+++ 04/Task6.cpp
@@ -10,20 +10,20 @@ int main()
     while (true) {
         cout << "Enter student marks to get their grade" << endl;
         cin >> marks;
-        if (marks < 49) {
+        if (marks <= 49) {
             cout << message << " Grade F" << endl;
         }
         else if (marks >= 50 && marks <= 59) {
-            cout << message << "Grade E" << endl;
-        }
-        else if (marks >= 60 && marks < 71) {
             cout << message << "Grade D" << endl;
         }
-        else if (marks >= 72 && marks <= 84) {
+        else if (marks >= 60 && marks < 71) {
             cout << message << "Grade C" << endl;
         }
-        else if (marks >= 85 && marks <= 100) {
+        else if (marks >= 72 && marks <= 84) {
             cout << message << "Grade B" << endl;
+        }
+        else if (marks >= 85 && marks <= 100) {
+            cout << message << "Grade A" << endl;
         }
         else {
             cout << "Invalid Input" << endl;
