aea5f934d2724bb9d6ea2e6a3b4403747ee36cd8
falconeri <hananeight@gmail.com>
2024-10-04 18:49:01 +0500

    drive15

--- 04/Task4.cpp
+++ 04/Task4.cpp
@@ -2,8 +2,32 @@
 
 using namespace std;
 
+void log(string message, bool newLine) {
+    newLine == true ? cout << message << "\n" : cout << message; // Ternary "?" statement
+}
+
 int main()
 {
+    int first;
+    int second;
+    log ("------------------", true);
+    log ("Get greater number", true);
+    log ("Input first number : ", false);
+    cin >> first;
+    log ("Input second number : ", false);
+    cin >> second;
 
+    if (first > second)
+    {
+        log (first + " is greater than " + second, true);
+    }
+    else if (first < second)
+    {
+        log (second + " is greater than " + first, true);
+    }
+    else
+    {
+        log ("Invalid inputs", true);
+    }
     return 0;
 }
