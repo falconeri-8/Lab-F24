8d030d39f7874c6c3f1124726b729c97f113bbbc
falconeri <hananeight@gmail.com>
2024-10-04 22:22:51 +0500

    updated 04/08

---
+++ 04/Task8.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void log(string message, bool newLine) {
+    newLine == true ? cout << message << "\n" : cout << message;
+}
+
+
+int main()
+{
+    char input = 'a';
+    log ("Input a value : ", false);
+    cin >> input;
+
+    if (isdigit(input))
+    {
+        input%2==0 ? log("Even Digit", true) : log("Odd digit", true);
+    }
+    else if (isalpha(input))
+    {
+        isupper(input) ? log("Uppercase Alphabet", true) : log("Lowercase Alphabet", true);
+    }
+    else
+    {
+        log ("We have a special symbol", true);
+    }
+
+}
