826ecaf51143ad75ca84d5629ae3dee05eb90a5a
falconeri <hananeight@gmail.com>
2024-10-07 19:52:58 +0500

    task3

--- 05/Task3.cpp
+++ 05/Task3.cpp
@@ -1,3 +1,22 @@
-//
-// Created by falconeri on 10/7/24.
-//
+#include <iostream>
+
+using namespace std;
+
+void log(string message, bool newLine) {
+    newLine == true ? cout << message << "\n" : cout << message;
+}
+
+int main()
+{
+    int sum, num;
+    log("Input 10 integers to get their average", true);
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cin >> num;
+        sum += num;
+    }
+
+    log("The average is " + to_string(sum/10), true);
+
+}
