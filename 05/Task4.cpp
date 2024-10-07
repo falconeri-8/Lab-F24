b75546765ced700d869ce9c68764f66f14294c90
falconeri <hananeight@gmail.com>
2024-10-07 20:23:16 +0500

    task4

--- 05/Task4.cpp
+++ 05/Task4.cpp
@@ -1,3 +1,44 @@
-//
-// Created by falconeri on 10/7/24.
-//
+#include <iostream>
+
+using namespace std;
+
+void log(string message, bool newLine) {
+    newLine == true ? cout << message << "\n" : cout << message;
+}
+
+int main()
+{
+    log("Enter Positive integers to get count, min, max & average", true);
+    log("Get Result with -1", true);
+
+    int input, min, max, sum, count;
+
+    while (true)
+    {
+        cin >> input;
+        if (input == -1) {
+            log ("Count = " + to_string(count) + "", true);
+            log ("Minimum = " + to_string(min) + "", true);
+            log ("Maximum = " + to_string(max) + "", true);
+            log ("Average = " + to_string(sum/10) + "", true);
+            break;
+        }
+        if (input <= -2 || input == 0)
+        {
+            log ("Enter positive integers", true);
+        }
+        else {
+            count += 1;
+            sum += input;
+            if (min > input)
+            {
+                min = input;
+            }
+            if (max < input)
+            {
+                max = input;
+            }
+        }
+    }
+
+}
--- 05/Task5.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by falconeri on 10/7/24.
-//
