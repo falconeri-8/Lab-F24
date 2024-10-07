d267f5420329e2978c0cefa7a45b9b0819c5c37d
falconeri <hananeight@gmail.com>
2024-10-07 22:53:53 +0500

    05/05 up.

--- 05/Task5.cpp
+++ 05/Task5.cpp
@@ -6,30 +6,34 @@ void log(string message, bool newLine) {
     newLine == true ? cout << message << "\n" : cout << message;
 }
 
+bool prime(int n) {
+    for (int i = 2; i*i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    // Prime numbers ONLY divisible by itself
+    return true;
+}
+
 int main()
 {
     int input;
 
-    log("Hello, World!", true);
+    log("Prime number check", true);
+    log("Input integer", true);
 
-    while (true)
-    {
-
-        if (input <= 1)
-        {
+    while (true) {
+        cin >> input;
+        if (input <= 1) {
             log("Process ended", true);
             break;
         }
-        else
-        {
-            // Prime numbers ONLY divisible by itself
-           for (int i = 2; i <= input; i++)
-           {
-               if (input % i == 0)
-               {
-                   log (input + " is not a prime number", true);
-               }
-           }
+
+        if (prime(input)) {
+            log (to_string(input) + " is a prime number", true);
+        } else {
+            log (to_string(input) + " is NOT a prime number", true);
         }
     }
 
