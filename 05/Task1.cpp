e2c00f311ef6a8bc0881e1cf3e9566366c55c8b6
falconeri <hananeight@gmail.com>
2024-10-07 18:58:55 +0500

    lab04 pub.

--- 05/Task1.cpp
+++ 05/Task1.cpp
@@ -1,3 +1,33 @@
-//
-// Created by falconeri on 10/7/24.
-//
+#include <iostream>
+
+using namespace std;
+
+void log(string message, bool newLine)
+{
+    if (newLine) {
+        cout << message << endl;
+    }
+    else {
+        cout << message;
+    }
+}
+
+int getFactorial(int n)
+{
+    int factorial = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        factorial *= i;
+    }
+    return factorial;
+}
+
+int main()
+{
+    int number;
+    log("Input Integer to get the factorial", true);
+
+    cin >> number;
+    log(to_string(getFactorial(number)), true);
+
+}
