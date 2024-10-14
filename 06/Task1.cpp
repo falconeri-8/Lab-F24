95538a053eff3cf358979c5ee03b6ba2da8ec039
falconeri <hananeight@gmail.com>
2024-10-14 19:11:25 +0500

    01

--- 06/Task1.cpp
+++ 06/Task1.cpp
@@ -1,3 +1,20 @@
-//
-// Created by falconeri on 10/14/24.
-//
+#include <iostream>
+
+using namespace std;
+
+void log(string message, bool newLine) {
+    newLine == true ? cout << message << "\n" : cout << message; // Ternary "?" statement
+}
+
+int getVolume(int radius)
+{
+    return (1.33 * 3.14 * (radius * radius * radius));
+}
+
+int main()
+{
+    log ("Enter radius of sphere to get its volume", true);
+    int radius;
+    cin >> radius;
+    cout << "Volume of sphere is " << getVolume(radius) << endl;
+}
