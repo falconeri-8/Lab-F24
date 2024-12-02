640514823670ca85eb1375e7239a4bcbca9309db
falconeri <hananeight@gmail.com>
2024-12-02 22:44:47 +0500

    ttt

--- Foundations/TicTacToe.cpp
+++ Foundations/TicTacToe.cpp
@@ -3,92 +3,65 @@
 // Contributions are appreciated
 
 #include <iostream>
-
 using namespace std;
 
-void log(string message, bool newLine) {
-	newLine == true ? cout << message << "\n" : cout << message; // Ternary "?" statement
-}
-
 class Map {
+private:
+	int array[3][3] = { {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1} };
+	string display[3][3] = { {"-", "-", "-"}, {"-", "-", "-"}, {"-", "-", "-"} };
 
-	private:
-		bool catchStreak(int _1, int _2, int _3) {
-			if (_1 == _2 && _2 == _3 && _1 != -1 && _2 != -1 && _3 != -1) {
+	bool catchStreak(int a, int b, int c) {
+		return (a == b && b == c && a != -1);
+	}
+
+public:
+	void Generate() const {
+		for (auto &row : display)
+			cout << row[0] << " | " << row[1] << " | " << row[2] << "\n";
+	}
+
+	bool Streak() {
+		for (int i = 0; i < 3; i++) {
+			if (catchStreak(array[i][0], array[i][1], array[i][2]) || catchStreak(array[0][i], array[1][i], array[2][i]))
 				return true;
-			}
-			return false;
 		}
+		return catchStreak(array[0][0], array[1][1], array[2][2]) || catchStreak(array[0][2], array[1][1], array[2][0]);
+	}
 
-	public:
-		int array[9] = {-1,-1,-1,-1,-1,-1,-1,-1,-1};
-		string display[9] = {"-","-","-","-","-","-","-","-","-",};
-
-
-
-		void Generate() const {
-			log (display[0] + " | " + display[1] + " | " + display[2] , true);
-			log (display[3] + " | " + display[4] + " | " + display[5] , true);
-			log (display[6] + " | " + display[7] + " | " + display[8] , true);
-		}
-
-		bool Streak() {
-
-			if (catchStreak(array[0], array[1], array[2])) { return true; }
-			if (catchStreak(array[3], array[4], array[5])) { return true; }
-			if (catchStreak(array[6], array[7], array[8])) { return true; }
-
-			if (catchStreak(array[0], array[3], array[6])) { return true; }
-			if (catchStreak(array[1], array[4], array[7])) { return true; }
-			if (catchStreak(array[2], array[5], array[8])) { return true; }
-
-			if (catchStreak(array[0], array[4], array[8])) { return true; }
-			if (catchStreak(array[2], array[4], array[6])) { return true; }
-
-			return false;
-		}
+	bool MarkTile(int tile, int player) {
+		if (tile < 1 || tile > 9) return false; // Ensure valid tile input
+		int row = (tile - 1) / 3, col = (tile - 1) % 3;
+		if (array[row][col] != -1) return false; // Tile already marked
+		array[row][col] = player;
+		display[row][col] = player == 0 ? "O" : "1";
+		return true;
+	}
 };
 
 int main() {
-	int player = 0;
-	int tile;
+	Map game;
+	int player = 0, tile;
 
-	Map Map;
-
-	log ("Playing TicTacToe with Player 0 & Player 1", true);
-	log ("Enter 1 - 9 to mark your tile", true);
-	log ("1 | 2 | 3", true);
-	log ("4 | 5 | 6", true);
-	log ("7 | 8 | 9", true);
-
+	cout << "Tic-Tac-Toe\nEnter 1-9 to mark your tile:\n1 | 2 | 3\n4 | 5 | 6\n7 | 8 | 9\n";
 	for (int i = 0; i < 9; i++) {
+		game.Generate();
+		cout << "Player " << player << "'s turn. Enter a tile number: ";
 		cin >> tile;
 
-		if (Map.array[tile - 1] != -1)
-		{
-			log ("Tile " + to_string(tile) + " is already marked" , true);
+		if (!game.MarkTile(tile, player)) {
+			cout << "Invalid move. Try again.\n";
 			--i;
 			continue;
 		}
 
-		if (player == 1) {
-			Map.display[tile - 1] = "1";
-			Map.array[tile - 1] = 1;
-			player = 0;
-		} else if (player == 0) {
-			Map.display[tile - 1] = "O";
-			Map.array[tile - 1] = 0;
-			player = 1;
+		if (game.Streak()) {
+			game.Generate();
+			cout << "Player " << player << " wins!\n";
+			return 0;
 		}
-
-		Map.Generate();
-		if (Map.Streak() == true)
-		{
-			log ("Streak", true);
-			break;
-		}
+		player = 1 - player;
 	}
 
-	log ("Game ended", true);
-
+	cout << "It's a draw!\n";
+	return 0;
 }
